9a7b07159808d0dc18f6ee52c5e93704328c3a62
Maxi Vadora <maxivadora@gmail.com>
2018-11-04 10:12:00 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p> Hola Mundo </p>
+      </div>
+    );
+  }
+}
+
+export default App;
